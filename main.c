853d491ca615389f6b6cb534124ce8701bb0dec9
Jimmy254-weks <jamesweks2019@gmail.com>
2023-10-25 07:55:37 -0700

    0x19. C - Stacks, Queues - LIFO, FIFO

--- main.c
+++ main.c
@@ -2,11 +2,12 @@
 stack_t *head = NULL;
 
 /**
-* main - Entry point of the program.
-* @argc: Number of command-line arguments.
-* @argv: Array of command-line argument strings.
-* Return: Always returns 0.
+* main - entry point
+* @argc: arguments count
+* @argv: list of arguments
+* Return: always 0
 */
+
 int main(int argc, char *argv[])
 {
 if (argc != 2)
@@ -20,16 +21,17 @@ return (0);
 }
 
 /**
-* create_node - Create a new stack node.
-* @n: Value to be stored in the node.
-* Return: Pointer to the newly created node, or NULL on failure.
+* create_node - Creates a node.
+* @n: Number to go inside the node.
+* Return: Upon sucess a pointer to the node. Otherwise NULL.
 */
 stack_t *create_node(int n)
 {
 stack_t *node;
+
 node = malloc(sizeof(stack_t));
 if (node == NULL)
-exit(EXIT_FAILURE);
+err(4);
 node->next = NULL;
 node->prev = NULL;
 node->n = n;
@@ -37,13 +39,15 @@ return (node);
 }
 
 /**
-* free_nodes - Free all nodes in the stack.
+* free_nodes - Frees nodes in the stack.
 */
 void free_nodes(void)
 {
 stack_t *tmp;
+
 if (head == NULL)
 return;
+
 while (head != NULL)
 {
 tmp = head;
@@ -52,14 +56,16 @@ free(tmp);
 }
 }
 
+
 /**
-* add_to_queue - Add a new node to the queue.
-* @new_node: Pointer to the new node to be added.
-* @ln: Line number of the opcode.
+* add_to_queue - Adds a node to the queue.
+* @new_node: Pointer to the new node.
+* @ln: line number of the opcode.
 */
-void add_to_queue(stack_t **new_node, __attribute__((unused)) unsigned int ln)
+void add_to_queue(stack_t **new_node, __attribute__((unused))unsigned int ln)
 {
 stack_t *tmp;
+
 if (new_node == NULL || *new_node == NULL)
 exit(EXIT_FAILURE);
 if (head == NULL)
@@ -70,6 +76,8 @@ return;
 tmp = head;
 while (tmp->next != NULL)
 tmp = tmp->next;
+
 tmp->next = *new_node;
 (*new_node)->prev = tmp;
+
 }
